327173320b95109d6612c0c4915ddd6983e8281c
totally0waffle <59709815+totally0waffle@users.noreply.github.com>
2026-01-30 11:41:15 -0700

    Fixed bug with configuration

--- bgremover.py
+++ bgremover.py
@@ -10,9 +10,10 @@ image_path = input("enter filename: ")    # Update with your actual path
 original_image = Image.open(image_path).convert("RGBA")
 new_data = []
 for item in original_image.getdata():
-    if item[0] > 254 and item[1] > 254 and item[2] > 254:
+    if item[0] > R_VAL and item[1] > G_VAL and item[2] > B_VAL:
         new_data.append((255, 255, 255, 0))     # Make white transparent
     else:
         new_data.append(item)
 original_image.putdata(new_data)
+
 original_image.save((image_path + ".png"), "PNG")    # Save as PNG with transparency
